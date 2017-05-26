49ed5ddd89d54e0423ca68c9f3f1b744716609ed
Jose Rojas <jose23rojas@unitec.edu>
2017-05-26 15:42:20 -0600

    solving merge

--- Main.cpp
+++ Main.cpp
@@ -31,24 +31,84 @@ int main(){
 	    cin>>contra;
 	      
         }
-        if(opcion==2){
-            int opcion2;
-	    while(opcion2 != 5){
-		   cout<<"MENU"<<endl;
-                   cout<<"1.) ADMIN"<<endl;
-	           cout<<"2.) Chef"<<endl;
-	           cout<<"3.) Lavaplatos"<<endl;
-	           cout<<"4.) Mesero"<<endl;
-	           cout<<"5.) Salir"<<endl;
-		   cin>>opcion2;
-	 
-	    }//fin while opcion2
+        if(opcion==2) // LOGIN PERSONAL
+        {
+        	string username, password, nombre, ID, numero;
+        	int edad, anoContratacion;
+
+        	// USUARIO
+        	// username
+        	cout << "Ingrese un nombre de usuario: ";
+        	cin >> username;
+
+        	// password
+        	cout << "Ingrese una contrasena: ";
+        	cin >> password;
+
+        	// nombre
+        	cout << "Ingrese un nombre: ";
+        	cin >> nombre;
+
+        	// edad
+        	cout << "Ingrese una edad: ";
+        	cin >> edad;
+        	// TODO: Validar que sea mayor de edad.
+
+        	// ID
+        	cout << "Ingrese un ID: ";
+        	cin >> ID;
+        	// TODO: Validar que sea unico.
+
+        	// numero
+        	cout << "Ingrese un numero de telefono: ";
+        	cin >> numero;
+        	// TODO: Validar que tenga por lo menos 8 digitos.
+
+        	// PERSONAL
+        	// anoContratacion
+        	cout << "Ingrese el ano de contratacion: ";
+        	cin >> anoContratacion;
+        	// TODO: Validar que tenga sentido.
+
+            int opcion2 = 0;
+	    	while(opcion2 != 5)
+	    	{
+		   		cout << "LOGIN PERSONAL" << endl;
+                cout << "1. ADMIN" << endl;
+	           	cout << "2. Chef" << endl;
+	           	cout << "3. Lavaplatos" << endl;
+	           	cout << "4. Mesero" << endl;
+	           	cout << "5. Salir" << endl;
+	           	cout << "Escoja una opcion: ";
+		   		cin >> opcion2;
+
+		   		if(opcion2 == 1) // Administrador
+		   		{
+
+		   		}
+		   		else if(opcion2 == 2) // Chef
+		   		{
+
+		   		}
+		   		else if(opcion2 == 3) // Lavaplatos
+		   		{
+
+		   		}
+		   		else if(opcion2 == 4) // Mesero
+		   		{
+
+		   		}
+		   		else
+		   		{
+		   			cout << "Debe escoger una opcion valida." << endl;
+		   		}
+
+	    	} // fin while opcion2
         }
-    }//fin while
+    } // fin while
 for(int i=0; i<usuarios.size();i++){
    delete usuarios[i];
 }
 usuarios.clear();
 return 0;
 }
-
